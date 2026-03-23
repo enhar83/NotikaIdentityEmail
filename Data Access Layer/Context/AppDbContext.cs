1ce067db0fdd3e260f34e2b27ffcdd0976334833
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-03-23 20:54:06 +0300

    DbContext yapısı oluşturuldu, program.cs içerisinde tanımlandı. Dbye migration atıldı ve db oluşturuldu.

---
+++ Data Access Layer/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data_Access_Layer.Context
+{
+    //adı direkt olarak DbContext yapılınca migration atarken more than one DbContext found hatası gelir, bundan dolayı AppDbContext yapıldı
+    public class AppDbContext:IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
--- NotikaIdentityEmail/Program.cs
+++ NotikaIdentityEmail/Program.cs
@@ -1,7 +1,15 @@
+using Data_Access_Layer.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
