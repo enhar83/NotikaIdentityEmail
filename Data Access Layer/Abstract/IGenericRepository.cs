dcc89bb621dee124c8a443de473aded6b46459a0
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-03-23 22:34:52 +0300

    DAL katmanı içerisinde IGenericRepository ve GenericRepository sınıfları yazıldı.

---
+++ Data Access Layer/Abstract/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_Layer.Abstract
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<List<T>> GetListAsync();
+        Task<T?> GetByIdAsync(string id);
+        Task InsertAsync(T entity);
+        void Update(T entity); //ef core içerisinde update genellikle asenkron yapıda değildir.
+        void Delete(T entity);
+    }
+}
---
+++ Data Access Layer/Concrete/GenericRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data_Access_Layer.Abstract;
+using Data_Access_Layer.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data_Access_Layer.Concrete
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        //protected olması sadece bu sınıf ve bu sınıftan miras alanların erişilebilir olması anlamına geliyor. private yapılsaydı ileride yazılacak olan ProductRepository bu değişkenleri kullanamazdı.
+        //readonly olması ise _db nesnesinin sadece contructor içinde atanabileceğini kodun başka hiçbir yerinde yanlışlıkla değiştirilemeyeceğini garanti eder. (güvenlik önlemi)
+        protected readonly AppDbContext _db;
+        protected DbSet<T> _dbSet;
+
+        //db'yi tüm bina, _dbSet'i ise o binadaki bir oda gibi düşünebilirsin.
+        public GenericRepository(AppDbContext db)
+        {
+            _db = db;
+            _dbSet = db.Set<T>(); // Eğer T yerine AppUser gelirse, bu satır dbdeki AspNetUser tablosuna yol açar. Artık her metotta _db.Set<AppUser>() yazmak yerine direkt olarak _dbSet yazılır.
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        //burada eğer id kontrolü yapılmazsa sarı ünlem (posible null reference döner) nedeni ise metot tanımlanırken bir değer döneceği garanti veriliyor ama null dönme ihtimali de var. bu bir çelişki yaratıyor.
+        //bu durumdan kurtulmak için Task<T?> ile Id yanlış ise null dönebilir diye sisteme haber vermektir. 
+        public async Task<T?> GetByIdAsync(string id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<List<T>> GetListAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task InsertAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        //async olmamasının nedeni sadece nesnenin state'ini işaretlemesi içindir. dbye gideceği zaman UoW'nin SaveChanges metodu devreye girecek ve o zaman hallolacaktır.
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+    }
+}
