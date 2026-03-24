da817d94d93aa6da56e2e8182cf61c903f51300a
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-03-24 16:35:57 +0300

    Navbar alanı ViewComponent içerisine taşındı.

---
+++ NotikaIdentityEmail/ViewComponents/UserLayoutViewComponents/_UserLayoutNavbarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NotikaIdentityEmail.ViewComponents.UserLayoutViewComponents
+{
+    public class _UserLayoutNavbarComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
