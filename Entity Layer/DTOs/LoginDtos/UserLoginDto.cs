0444b47a935576eacfc3a70407ce706f88a6135e
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-03-24 14:29:48 +0300

    Login işlemi için gerekli olan Dto DTOs klaösürüne açıldı.

---
+++ Entity Layer/DTOs/LoginDtos/UserLoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Layer.DTOs.LoginDtos
+{
+    public class UserLoginDto
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}
---
+++ NotikaIdentityEmail/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NotikaIdentityEmail.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Signin()
+        {
+            return View();
+        }
+    }
+}
