4e23ce4ea7f8c5ea462d17790dc801b049b0f466
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-03-24 16:55:50 +0300

    MessageSidebarComponentPartial oluşturuldu, inbox içerisindeki sidebar buraya konuldu.

---
+++ NotikaIdentityEmail/ViewComponents/MessageViewComponents/_MessageSidebarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NotikaIdentityEmail.ViewComponents.MessageViewComponents
+{
+    public class _MessageSidebarComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
