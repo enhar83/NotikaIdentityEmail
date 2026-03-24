f9a3462649c3718527089149f3575a323439d1d7
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-03-24 09:48:05 +0300

    Business katmanı içerisine FluentValidation katmanı açıldı, UserRegisterDto için gerekli kurallar yazıldı, prgram.cs içerisinde kayıt tamamlandı.

--- Business Layer/ValidationRules/AppUserValidationRules/UserRegisterValidator.cs
+++ Business Layer/ValidationRules/AppUserValidationRules/UserRegisterValidator.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entity_Layer.DTOs.RegisterDtos;
+using FluentValidation;
 
 namespace Business_Layer.ValidationRules.AppUserValidationRules
 {
-    public class UserRegisterValidator
+    public class UserRegisterValidator:AbstractValidator<UserRegisterDto>
     {
+        public UserRegisterValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Ad alanı boş geçilemez.")
+                .MaximumLength(50).WithMessage("Ad alanı en fazla 50 karakter olabilir");
+
+            RuleFor(x => x.Surname)
+                .NotEmpty().WithMessage("Soyad alanı boş geçilemez.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email adresi gereklidir.")
+                .EmailAddress().WithMessage("Lütfen geçerli bir e-posta adresi giriniz.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Şifre boş geçilemez.")
+                .MinimumLength(6).WithMessage("Şifre en az 6 karakter olmalıdır.");
+
+            RuleFor(x => x.ConfirmPassword)
+                .Equal(x => x.Password).WithMessage("Şifreler birbiriyle uyuşmuyor.");
+        }
     }
 }
+
+/*
+    FluentValidation Nedir?
+        - bir web siten olduğunu düşün ve kullanıcıların kayıt olduğunu düşün. kullanıcu adı boş olmasın, şifre en az 6 karakterde az olmmasın gibi kuralların var.
+            * Eski Yöntem: Controller içinde sürekli eğer şu boşşsa hata var eğer bu kısaysa hata var diye kod yazarsın. bu da içinden çıkalamaz bir hal alır.
+            * FluentValidation Yöntemi: Doğrulama kurallarını ana koddan tamamen ayırır. bir kural listesi oluşturursun ve sistem bir veri geldiğinde otomatik olarak o listeye bakar.
+*/
--- NotikaIdentityEmail/Program.cs
+++ NotikaIdentityEmail/Program.cs
@@ -1,6 +1,7 @@
 using Business_Layer.Abstract;
 using Business_Layer.Concrete;
 using Business_Layer.Mappings;
+using Business_Layer.ValidationRules.AppUserValidationRules;
 using Data_Access_Layer.Abstract;
 using Data_Access_Layer.Concrete;
 using Data_Access_Layer.Context;
@@ -22,7 +23,7 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 builder.Services.AddScoped(typeof(IGenericService<>), typeof(GenericManager<>));
 builder.Services.AddAutoMapper(typeof(GeneralMapping)); //AutoMapper.Extensions.Microsoft.DependencyInjection paketi kurulmazsa hata alýnýr.
-
+builder.Services.AddValidatorsFromAssemblyContaining<UserRegisterValidator>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
