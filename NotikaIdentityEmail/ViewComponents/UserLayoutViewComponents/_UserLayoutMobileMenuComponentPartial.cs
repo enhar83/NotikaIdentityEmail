ea5b0bd2f54700631a02877ea2ec847d401a74f8
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-03-24 16:39:57 +0300

    Mobile menu kısmı ViewComponent içerisine taşındı.

---
+++ NotikaIdentityEmail/ViewComponents/UserLayoutViewComponents/_UserLayoutMobileMenuComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NotikaIdentityEmail.ViewComponents.UserLayoutViewComponents
+{
+    public class _UserLayoutMobileMenuComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
