aaf3c6a7f1884603db40b702b200f1e3279e7c6b
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-03-24 16:30:07 +0300

    Header alanı ViewComponent içerisine taşındı.

---
+++ NotikaIdentityEmail/ViewComponents/UserLayoutViewComponents/_UserLayoutHeaderComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NotikaIdentityEmail.ViewComponents.UserLayoutViewComponents
+{
+    public class _UserLayoutHeaderComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
