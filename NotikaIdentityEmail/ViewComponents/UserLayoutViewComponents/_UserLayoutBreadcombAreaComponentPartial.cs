3242b8757c78fdb913931c4c56c6c06ac9f1758d
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-03-24 16:43:11 +0300

    Breadcomb area ViewComponent içerisine taşındı.

---
+++ NotikaIdentityEmail/ViewComponents/UserLayoutViewComponents/_UserLayoutBreadcombAreaComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NotikaIdentityEmail.ViewComponents.UserLayoutViewComponents
+{
+    public class _UserLayoutBreadcombAreaComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
