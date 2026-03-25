35f429b6417329ef6993b7cdc0fb9161b184ece8
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-03-25 09:10:54 +0300

    Category entitysi eklendi.

---
+++ Entity Layer/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Layer.Entities
+{
+    public class Category
+    {
+        public Guid MyProperty { get; set; }
+        public string? CategoryName { get; set; }
+        public string? CategoryStringUrl { get; set; }
+        public bool? CategoryStatus { get; set; }
+    }
+}
