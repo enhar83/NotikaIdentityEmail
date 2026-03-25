8be560f8414ed5905cff38b80e50af5ae282b97f
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-03-25 11:47:02 +0300

    IMessageService ve MessageManager sınıfları oluşturuldu, içleri dolduruldu ve program.cs içerisinde kayıtları tamamlandı.

---
+++ Business Layer/Concrete/MessageManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Business_Layer.Abstract;
+using Data_Access_Layer.Abstract;
+using Entity_Layer.Entities;
+
+namespace Business_Layer.Concrete
+{
+    public class MessageManager : IMessageService
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly IMapper _mapper;
+
+        public MessageManager(IUnitOfWork uow, IMapper mapper)
+        {
+            _uow = uow;
+            _mapper = mapper;
+        }
+
+        public void TDelete(Message entity)
+        {
+            _uow.Messages.Delete(entity);
+        }
+
+        public async Task<Message?> TGetByIdAsync(Guid id)
+        {
+            return await _uow.Messages.GetByIdAsync(id);
+        }
+
+        public Task<List<Message>> TGetListAsync()
+        {
+            return _uow.Messages.GetListAsync();   
+        }
+
+        public async Task TInsertAsync(Message entity)
+        {
+            await _uow.Messages.InsertAsync(entity);
+        }
+
+        public void TUpdate(Message entity)
+        {
+            _uow.Messages.Update(entity);
+        }
+    }
+}
--- NotikaIdentityEmail/Program.cs
+++ NotikaIdentityEmail/Program.cs
@@ -39,6 +39,7 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IGenericService<>), typeof(GenericManager<>));
 builder.Services.AddScoped<IAppUserService, AppUserManager>();
 builder.Services.AddScoped<ICategoryService, CategoryManager>();
+builder.Services.AddScoped<IMessageService, MessageManager>();
 
 builder.Services.AddAutoMapper(typeof(GeneralMapping)); //AutoMapper.Extensions.Microsoft.DependencyInjection paketi kurulmazsa hata alýnýr.
 
