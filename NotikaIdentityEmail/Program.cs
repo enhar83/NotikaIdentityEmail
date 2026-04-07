73e54de80f6169f16177bfa701c19082e69ad823
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-04-07 20:29:12 +0300

    Rol işlemleri için RoleController, IAppRoleService ve AppRollManager oluşturuldu, program.cs içerisinde kayıtı tamamlandı. içleri henüz doldurulmadı

--- NotikaIdentityEmail/Program.cs
+++ NotikaIdentityEmail/Program.cs
@@ -45,6 +45,7 @@ builder.Services.AddScoped<IAppUserService, AppUserManager>();
 builder.Services.AddScoped<ICategoryService, CategoryManager>();
 builder.Services.AddScoped<IMessageService, MessageManager>();
 builder.Services.AddScoped<IEmailActivationService, EmailActivationManager>();
+builder.Services.AddScoped<IAppRoleService, AppRoleManager>();
 
 builder.Services.AddAutoMapper(typeof(GeneralMapping)); //AutoMapper.Extensions.Microsoft.DependencyInjection paketi kurulmazsa hata alýnýr.
 
