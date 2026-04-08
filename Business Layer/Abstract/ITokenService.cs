941e05239b1cf4ccc38e3765beef08c3abb57826
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-04-08 16:20:17 +0300

    ITokenService ve TokenManager içerisinde CreateToken metodu yazıldı.

--- Business Layer/Abstract/ITokenService.cs
+++ Business Layer/Abstract/ITokenService.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entity_Layer.Entities;
 
 namespace Business_Layer.Abstract
 {
     public interface ITokenService
     {
+        string CreateToken(AppUser user);
     }
 }
--- Business Layer/Concrete/TokenManager.cs
+++ Business Layer/Concrete/TokenManager.cs
@@ -1,13 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using Business_Layer.Abstract;
+using Business_Layer.Configurations;
+using Entity_Layer.Entities;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Business_Layer.Concrete
 {
     public class TokenManager:ITokenService
     {
+        private readonly JwtSettings _jwtSettings;
+
+        //IOptions<JwtSettings> sayesinde appsettings içerisindeki verileri buraya otomatik gelir.
+        public TokenManager(IOptions<JwtSettings> jwtSettings)
+        {
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        //bir kullanıcı alır ve ona karşılık bir Token (string) verir.
+        public string CreateToken(AppUser user)
+        {
+            //ilk olarak claimler hazırlanır. bir API endpointidir ve görevi kullanıcının gönderdiği bilgileri JWT claimlerine çevirmektir.
+            //Claim: Kullanıcıya dair iddia edilen bilgilerdir.
+            //Token çözüldüğünde bu bilgiler dbye gitmeden okunabilir.
+
+            var claims = new List<Claim>
+            {
+                //standart .net clamileri (authorize attributeları bunlar otomatil tanır)
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Email, user.Email!),
+                new Claim(ClaimTypes.Name, user.UserName!),
+
+                //custom claimler.
+                new Claim("name",user.Name),
+                new Claim("surname",user.Surname),
+                new Claim("city",user.City!),
+
+                //JTI: her tokena özel benzersiz bir id verir. güvenlik için önemlidir.
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            //şifreleme anahtarı oluşturulur.
+            //appsettings içerisindeki Key stringini byte dizisine çevirip simetrik bir anahtar yapma işlemidir.
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Key));
+
+            //bu anahtarı kullanarak HMAC-SHA256 algortmasıyla bir imzalama kimliği oluşturuluyor. Böylece JWT web token dijital olarak imzalanır. 
+            //bu mühür tokenın içeriği değişirse imzanın bozulmasını sağlar. 
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            //burada token nesnesi oluşturulur.
+            //tüm parçalar birleştirilir, kim bastı?, kimin için?, hangi bilgilerle?, ne kadar süreyle? 
+            var tokenOptions = new JwtSecurityToken(
+                issuer: _jwtSettings.Issuer, //yayınlayan (localhost)
+                audience: _jwtSettings.Audience, //alıcı (localhost)
+                claims: claims, //kullanıcı bilgileir
+                expires: DateTime.Now.AddMinutes(_jwtSettings.ExpireMinutes), //bitiş süresi
+                signingCredentials: creds //güvenlik mührü
+            );
+
+            //oluşturulan nesneyi Header.Payload.Signature formatında bir string olarak döner. 
+            return new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+        }
     }
 }
