554f96f725cbd3e5a8f3bc10662ced8213493cbc
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-04-13 16:10:06 +0300

    ICommentService ve CommentManager, IGenericService'ten miras alacak şekilde eklendi. program.cs içerisinde kayıdı yapıldı.

---
+++ Business Layer/Concrete/CommentManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Business_Layer.Abstract;
+using Data_Access_Layer.Abstract;
+using Entity_Layer.Entities;
+
+namespace Business_Layer.Concrete
+{
+    public class CommentManager : ICommentService
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly IMapper _mapper;
+        public CommentManager(IUnitOfWork uow, IMapper mapper)
+        {
+            _uow = uow;
+            _mapper = mapper;
+        }
+
+        public void TDelete(Comment entity)
+        {
+            _uow.Comments.Delete(entity);
+            _uow.SaveAsync();
+        }
+
+        public async Task<Comment?> TGetByIdAsync(Guid id)
+        {
+            return await _uow.Comments.GetByIdAsync(id);
+        }
+
+        public async Task<List<Comment>> TGetListAsync()
+        {
+            return await _uow.Comments.GetListAsync();
+        }
+
+        public async Task TInsertAsync(Comment entity)
+        {
+            await _uow.Comments.InsertAsync(entity);
+            await _uow.SaveAsync();
+        }
+
+        public void TUpdate(Comment entity)
+        {
+            _uow.Comments.Update(entity);
+            _uow.SaveAsync();
+        }
+    }
+}
