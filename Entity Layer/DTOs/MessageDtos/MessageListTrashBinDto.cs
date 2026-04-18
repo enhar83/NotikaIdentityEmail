99f5de7c85ae94304babf605d86d4a12fa213ad6
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-04-19 00:09:33 +0300

    MessageListTrashBinDto oluşturuldu, mapping kodları yazıldı.

---
+++ Entity Layer/DTOs/MessageDtos/MessageListTrashBinDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Layer.DTOs.MessageDtos
+{
+    public class MessageListTrashBinDto
+    {
+        public Guid Id { get; set; }
+        public string? SenderName { get; set; }
+        public string? CategoryName { get; set; }
+        public string? Subject { get; set; }
+        public DateTime SendDate { get; set; }
+        public bool IsRead { get; set; }
+    }
+}
